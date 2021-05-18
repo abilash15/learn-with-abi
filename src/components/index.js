be25f8c4acb8ab4337c69374ad36aaa971066a9d
abilash15 <79013483+abilash15@users.noreply.github.com>
2021-05-18 13:30:27 +0530

    home page slide

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,1 @@
-export { default as QuarklycommunityKitMobileSidePanel } from "./QuarklycommunityKitMobileSidePanel"
 export { default as QuarklycommunityKitBackToTop } from "./QuarklycommunityKitBackToTop"
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,11 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Link, Strong, Text, Box } from "@quarkly/widgets";
+import { Theme, Link, Text, Icon, Box } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { GlobalQuarklyPageStyles } from "global-page-styles";
-import { Override, Menu, Section } from "@quarkly/components";
+import { Override, StackItem, Stack, Section } from "@quarkly/components";
 import * as Components from "components";
+import { MdArrowDownward } from "react-icons/md";
 export default (() => {
 	return <Theme theme={theme}>
 		<GlobalQuarklyPageStyles pageUrl={"blog"} />
@@ -15,58 +16,33 @@ export default (() => {
 			<meta name={"description"} content={"Web site created using quarkly.io"} />
 			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/6027e9d3977a55001e57bde9/images/SPRK_default_preset_name_custom%20%E2%80%93%201.png?v=2021-05-18T05:30:14.939Z"} type={"image/x-icon"} />
 		</Helmet>
-		<Components.QuarklycommunityKitMobileSidePanel>
-			<Section>
-				<Box
-					display="flex"
-					padding="12px 0"
-					justify-content="space-between"
-					align-items="center"
-					flex-direction="row"
-					md-flex-direction="column"
-				>
-					<Text margin="0" md-margin="0px 0 20px 0" text-align="left" font="--lead">
-						<Strong>
-							Learn with ABI
-						</Strong>
-					</Text>
-					<Menu
-						display="flex"
-						justify-content="center"
-						font="--base"
-						font-weight="700"
-						md-flex-direction="column"
-						md-align-items="center"
-					>
-						<Override slot="link" text-decoration="none" color="--dark" padding="6px 12px" />
-						<Override slot="link-active" color="--primary" />
-						<Override slot="item" padding="6px" />
-					</Menu>
-				</Box>
-			</Section>
-		</Components.QuarklycommunityKitMobileSidePanel>
 		<Components.QuarklycommunityKitBackToTop />
-		<Section padding="140px 0" sm-padding="40px 0" background="url(https://images.unsplash.com/photo-1515600051222-a3c338ff16f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80) center/cover">
-			<Box max-width="240px" padding="50px 80px 80px 50px" background="--color-light" color="--dark">
-				<Text
-					as="h4"
-					font="--base"
-					color="--grey"
-					letter-spacing="1px"
-					text-transform="uppercase"
-					margin="6px 0"
-				>
-					About me
+		<Components.QuarklycommunityKitBackToTop />
+		<Section
+			background="linear-gradient(0deg,rgba(4, 8, 12, 0.6) 0%,rgba(4, 8, 12, 0.6) 100%),--color-darkL2 url(https://images.unsplash.com/photo-1510125594188-5afc74c8cc43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80) center/cover"
+			padding="64px 0"
+			sm-padding="40px 0"
+			color="--light"
+			font="--base"
+		>
+			<Stack>
+				<StackItem width="75%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text color="--lightD2" letter-spacing="1px" text-transform="uppercase" margin="0">
+						Excellence in everything
+					</Text>
+					<Text as="h1" font="--headline1" md-font="--headline2" margin="10px 0">
+						Striving for perfection in everything we do. Unparalleled service for everyone.
+					</Text>
+				</StackItem>
+			</Stack>
+			<Box text-align="center" margin="96px 0 0 0">
+				<Text margin="8px 0" text-transform="uppercase">
+					Who we are
 				</Text>
-				<Text as="h2" font="--headline2" margin="0 0 12px 0">
-					Hey I’m Abilash
-				</Text>
-				<Text font="--base">
-					My name is Abilash. Moore, born and raised in France, worked as a professional photographer and videographer for more than 10 years, awarded the CDS Documentary Essay Prize in 2018 and Best Photographer of The Year 2019 by Sony World Photography Awards.
-				</Text>
+				<Icon category="md" margin="0 auto" icon={MdArrowDownward} />
 			</Box>
 		</Section>
-		<Components.QuarklycommunityKitBackToTop />
 		<Link
 			font={"--capture"}
 			font-size={"10px"}
