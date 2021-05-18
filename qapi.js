bf8458234143f55f02bf76d2218cd704314b7e1c
abilash15 <79013483+abilash15@users.noreply.github.com>
2021-05-18 13:35:29 +0530

    web model

---
+++ qapi.js
@@ -0,0 +1,44 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6027e9d3977a55001e57bdec",
+				"6027e9d3977a55001e57bdee",
+				"6027eda68954e5001ef16300",
+				"60a210f17f5b95001e5ec7fe"
+			]
+		},
+		"6027e9d3977a55001e57bdec": {
+			"id": "6027e9d3977a55001e57bdec",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6027e9d3977a55001e57bdee": {
+			"id": "6027e9d3977a55001e57bdee",
+			"name": "index",
+			"pageUrl": "index"
+		},
+		"6027eda68954e5001ef16300": {
+			"id": "6027eda68954e5001ef16300",
+			"pageUrl": "blog",
+			"name": "blog"
+		},
+		"60a210f17f5b95001e5ec7fe": {
+			"id": "60a210f17f5b95001e5ec7fe",
+			"pageUrl": "books",
+			"name": "books"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {
+			"favicon_32px": "https://uploads.quarkly.io/6027e9d3977a55001e57bde9/images/SPRK_default_preset_name_custom%20%E2%80%93%201.png?v=2021-05-18T05:30:14.939Z",
+			"title": ""
+		}
+	}
+}
